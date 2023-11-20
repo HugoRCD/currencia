c6a53a9d7cd568c7c3a8bd97eb6873b60b8e013f
johann <johann.cavallucci@epitech.eu>
2023-11-20 12:12:34 +0100

    + Hugo a dit oui +

--- pages/app/articles.vue
+++ pages/app/articles.vue
@@ -6,7 +6,7 @@
     description="The Community displays the latest crypto news published by projects from all corners of the crypto world."
   >
     <div class="flex flex-col gap-3">
-      <ListActu></ListActu>
+      <NewsListActu />
     </div>
   </LayoutSectionWrapper>
 </template>
