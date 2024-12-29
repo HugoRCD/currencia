03d84e261b39bd8001cfb377d511ef68352baf5f
HugoRCD <hugo.richard@epitech.eu>
2024-12-29 11:15:52 +0100

    make websocket work

---
+++ apps/currencia/server/api/crypto/[symbol]/index.get.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+const symbolParams = z.object({
+  symbol: z.string(),
+})
+
+export default defineEventHandler(async (event) => {
+  const eventStream = createEventStream(event)
+  const { symbol } = await getValidatedRouterParams(event, symbolParams.parse)
+
+  const sendRandomNumber = () => generateRandomValue()
+
+  const interval = setInterval(async () => {
+    await eventStream.push(String(sendRandomNumber()))
+  }, 1500)
+
+  eventStream.onClosed(async () => {
+    clearInterval(interval)
+    await eventStream.close()
+  })
+
+  return eventStream.send()
+})
