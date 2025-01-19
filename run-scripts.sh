b9fb0324eca06a43282c1532c59b4f15302c21ca
HugoRCD <hugo.richard@epitech.eu>
2025-01-19 14:58:17 +0100

    feat: Dockerfile for scripts

--- run-scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm crypto-scripts run scripts/coinmarketcap/index.ts
-docker run --rm crypto-scripts run scripts/kraken/index.ts
-docker run --rm crypto-scripts run scripts/cryptocompare/index.ts
