896c750cc62d0fc89064d18b145cf09e7037a256
HugoRCD <hugo.richard@epitech.eu>
2025-01-19 20:55:03 +0100

    up

--- apps/currencia/app/components/StatCard.vue
+++ apps/currencia/app/components/StatCard.vue
@@ -28,20 +28,29 @@ defineProps({
 </script>
 
 <template>
-  <div class="select-none rounded-lg bg-gray-800/80 p-4 transition-all hover:bg-gray-800">
+  <UCard
+    class="select-none rounded-lg"
+    :ui="{
+      body: {
+        base: '',
+        background: '',
+        padding: 'px-4 py-5 sm:p-4'
+      },
+    }"
+  >
     <UTooltip v-if="tooltip" :text="tooltip">
       <div class="flex flex-col gap-2">
         <div class="flex items-center justify-between">
-          <span class="text-sm font-medium text-gray-400">{{ title }}</span>
-          <UIcon :name="icon" class="size-4 text-gray-500" />
+          <span class="text-sm font-medium text-neutral-600 dark:text-neutral-400">{{ title }}</span>
+          <UIcon :name="icon" class="size-4 text-neutral-500 dark:text-neutral-500" />
         </div>
         <div class="flex items-baseline gap-1">
           <span
             class="text-xl font-semibold"
             :class="{
-              'text-gray-200': status === 'neutral',
-              'text-green-500': status === 'positive',
-              'text-red-500': status === 'negative'
+              'text-neutral-800 dark:text-neutral-200': status === 'neutral',
+              'text-green-600 dark:text-green-500': status === 'positive',
+              'text-red-600 dark:text-red-500': status === 'negative'
             }"
           >
             {{ value }}{{ suffix }}
@@ -52,21 +61,21 @@ defineProps({
 
     <div v-else class="flex flex-col gap-2">
       <div class="flex items-center justify-between">
-        <span class="text-sm font-medium text-gray-400">{{ title }}</span>
-        <UIcon :name="icon" class="size-4 text-gray-500" />
+        <span class="text-sm font-medium text-neutral-600 dark:text-neutral-400">{{ title }}</span>
+        <UIcon :name="icon" class="size-4 text-neutral-500 dark:text-neutral-500" />
       </div>
       <div class="flex items-baseline gap-1">
         <span
           class="text-xl font-semibold"
           :class="{
-            'text-gray-200': status === 'neutral',
-            'text-green-500': status === 'positive',
-            'text-red-500': status === 'negative'
+            'text-neutral-800 dark:text-neutral-200': status === 'neutral',
+            'text-green-600 dark:text-green-500': status === 'positive',
+            'text-red-600 dark:text-red-500': status === 'negative'
           }"
         >
           {{ value }}{{ suffix }}
         </span>
       </div>
     </div>
-  </div>
+  </UCard>
 </template>
